c4682eb880da15c8e0adb00808c8b8504fe1a392
耿智鑫 <gengzx@gengzhixindeMacBook-Pro.local>
2020-05-12 17:31:30 +0800

    webpack 完善

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,9 @@
 const webpack = require('webpack');
 const baseWebpackConfig = require('./webpack.config');
 
+// CSS 压缩
+const optimizeCss = require('optimize-css-assets-webpack-plugin')
+
 // 清除打包路径
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -12,7 +15,16 @@ baseWebpackConfig.plugins.push(
     'process.env': {
       NODE_ENV: '"production"'
     }
+  }),
+
+  new optimizeCss({
+    cssProcessor: require('cssnano'), //引入cssnano配置压缩选项
+    cssProcessorOptions: { 
+      discardComments: { removeAll: true } 
+    },
+    canPrint: true //是否将插件信息打印到控制台
   })
+
   /*
   new webpack.optimize.UglifyJsPlugin({
     beautify: false,

f6754ce7a34703e98f0b83fc7fc6f8fffac477ab
耿智鑫 <gengzx@gengzhixindeMacBook-Pro.local>
2020-06-02 17:37:41 +0800

    加入 storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import { addParameters } from '@storybook/react';
+
+addParameters({
+    options: {
+        theme:{
+            brandTitle:'BSTD组件库',
+            brandUrl: '#'
+        },
+        /*
+        storySort: (a, b) =>
+        a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),*/
+    }
+});

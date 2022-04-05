cef58fe730b5a358f15cbee0351b4c6e6f8f3317
gengzx <gzx97620@126.com>
2022-04-05 20:37:02 +0800

    完善

--- webpack.config.js
+++ webpack.config.js
@@ -27,13 +27,36 @@ module.exports = {
 		commons: ['./src/utils'] // 提取公共文件
 	},
 
+	// optimization: {
+	// 	splitChunks: {
+	// 		cacheGroups: {
+	// 			vendors: {
+	// 				test: /[\\/]node_modules[\\/]/,
+	// 				priority: -10,
+	// 				name:"vendors"
+	// 			}
+	// 		}
+	// 	}
+	// },
+
+	// TODO: 待优化
 	optimization: {
 		splitChunks: {
 			cacheGroups: {
 				vendors: {
 					test: /[\\/]node_modules[\\/]/,
+					minChunks:2,
 					priority: -10,
-					name:"vendors"
+					name: 'vendors',
+					chunks: 'all' // async表示只从异步加载得模块（动态加载import()）里面进行拆分,initial：表示只从入口模块进行拆分,all：表示以上两者都包括
+				},
+				commons: {
+					test: /[\\/]src[\\/]utils[\\/]/,
+					name: 'commons',
+					minChunks: 2,
+					chunks: 'all',
+					priority: -1,
+					reuseExistingChunk: true 
 				}
 			}
 		}
